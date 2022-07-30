172d859309a462bcafa1e6c6f477d43fd863a031
chaosvulture <sven_dewit@outlook.com>
2022-07-30 21:34:47 +0200

    working on highlight buttons
     working on the highlight buttons :+1:

--- O2A2 - Visual Novel Jam/Assets/Scripts/HighlightButtons.cs
+++ O2A2 - Visual Novel Jam/Assets/Scripts/HighlightButtons.cs
@@ -7,14 +7,26 @@ public class HighlightButtons : MonoBehaviour
 {
     public Button[] buttons;
 
-    //private void Update()
-    //{
-    //   if (Input.GetMouseButtonDown(0) && DialogueManager.isDialogueBeingDisplayed == false)
-    //    {
-    //        foreach(Button b in buttons)
-    //        {
-    //            b.colors.pressedColor = ;
-    //        }
-    //    }
-    //}
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && DialogueManager.isDialogueBeingDisplayed == false)
+        {
+            foreach (Button b in buttons)
+            {
+                b.animator.Play("Highlighted");
+                Debug.Log("I work :)");
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && DialogueManager.isDialogueBeingDisplayed == false)
+        {
+            foreach (Button b in buttons)
+            {
+                b.animator.Play("Normal");
+                Debug.Log("I WORK!");
+            }
+        }
+
+    }
 }
